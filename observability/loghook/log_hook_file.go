35fc266903199f339749f012fa8b267916b38ced
SyaibanAhmadRamadhan <ibanrama29@gmail.com>
2025-11-15 22:50:12 +0700

    Refactor log rotation logic to separate error and standard log handling

--- observability/loghook/log_hook_file.go
+++ observability/loghook/log_hook_file.go
@@ -1,17 +1,45 @@
 package loghook
 
 import (
-	"io"
+	"fmt"
+	"strings"
 
 	"gopkg.in/natefinch/lumberjack.v2"
 )
 
-func NewRotatingWriter(filename string, maxSizeMB, maxBackups, maxAgeDays int, compress bool) io.Writer {
-	return &lumberjack.Logger{
-		Filename:   filename,
-		MaxSize:    maxSizeMB,  // in MB
-		MaxBackups: maxBackups, //  file backup
-		MaxAge:     maxAgeDays, // in days
-		Compress:   compress,
+type rotatingWriter struct {
+	errorLogFile *lumberjack.Logger
+	logFile      *lumberjack.Logger
+}
+
+func (w *rotatingWriter) Write(p []byte) (n int, err error) {
+	if strings.Contains(string(p), `"level":"ERROR"`) {
+		return w.errorLogFile.Write(p)
+	}
+
+	return w.logFile.Write(p)
+}
+
+func (w *rotatingWriter) Close() {
+	w.errorLogFile.Close()
+	w.logFile.Close()
+}
+
+func NewRotatingWriter(filename string, maxSizeMB, maxBackups, maxAgeDays int, compress bool) *rotatingWriter {
+	return &rotatingWriter{
+		logFile: &lumberjack.Logger{
+			Filename:   filename,
+			MaxSize:    maxSizeMB,  // in MB
+			MaxBackups: maxBackups, //  file backup
+			MaxAge:     maxAgeDays, // in days
+			Compress:   compress,
+		},
+		errorLogFile: &lumberjack.Logger{
+			Filename:   fmt.Sprintf("error_%s", filename),
+			MaxSize:    maxSizeMB,  // in MB
+			MaxBackups: maxBackups, //  file backup
+			MaxAge:     maxAgeDays, // in days
+			Compress:   compress,
+		},
 	}
 }
