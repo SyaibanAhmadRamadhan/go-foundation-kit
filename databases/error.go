0621068d5b4f995a78d8ca7b1e50eec72b013140
SyaibanAhmadRamadhan <ibanrama29@gmail.com>
2025-12-22 23:36:49 +0700

    feat(databases): add duplicate entry error handling for MySQL and PostgreSQL

--- databases/error.go
+++ databases/error.go
@@ -1,6 +1,11 @@
 package databases
 
-import "errors"
+import (
+	"errors"
+
+	"github.com/go-sql-driver/mysql"
+	"github.com/jackc/pgx/v5/pgconn"
+)
 
 // ErrNoRowFound is returned when a query expecting rows does not return any result.
 // Common for SELECT queries using QueryRow or similar.
@@ -13,3 +18,29 @@ var ErrNoUpdateRow = errors.New("no rows were updated")
 // ErrNoDeleteRow is returned when a DELETE operation does not affect any rows.
 // Usually means the specified criteria didn't match any existing record.
 var ErrNoDeleteRow = errors.New("no rows were deleted")
+
+// ErrDuplicateEntry is returned when an operation violates a uniqueness constraint,
+// such as attempting to insert a duplicate value into a column that requires unique values.
+var ErrDuplicateEntry = errors.New("duplicate entry")
+
+// MapDuplicateEntryError inspects the provided error and maps it to ErrDuplicateEntry
+// if it corresponds to a duplicate entry error from either PostgreSQL or MySQL.
+// If the error does not indicate a duplicate entry, it is returned unchanged.
+func MapDuplicateEntryError(err error) error {
+	if err == nil {
+		return nil
+	}
+
+	var pgErr *pgconn.PgError
+	if errors.As(err, &pgErr) {
+		if pgErr.Code == "23505" {
+			return ErrDuplicateEntry
+		}
+	}
+
+	var mysqlErr *mysql.MySQLError
+	if errors.As(err, &mysqlErr) && mysqlErr.Number == 1062 {
+		return ErrDuplicateEntry
+	}
+	return err
+}
--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/go-playground/locales v0.14.1
 	github.com/go-playground/universal-translator v0.18.1
 	github.com/go-playground/validator/v10 v10.26.0
+	github.com/go-sql-driver/mysql v1.9.3
 	github.com/golang-jwt/jwt/v5 v5.2.2
 	github.com/hashicorp/vault/api v1.22.0
 	github.com/jackc/pgx/v5 v5.7.5
@@ -37,6 +38,7 @@ require (
 )
 
 require (
+	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/bytedance/sonic v1.13.2 // indirect
 	github.com/bytedance/sonic/loader v0.2.4 // indirect
 	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
